eb25de0fc38fdc349c6703f5da2c86d10e2e2730
Madushan Gamage <madushanbsns@gmail.com>
2015-09-22 09:44:55 +0800

    Initial Commit

---
+++ BuildNumberTagger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuildNumberTagger
+//
+//  Created by Madushan Gamage on 9/22/15.
+//  Copyright (c) 2015 Hapticus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
