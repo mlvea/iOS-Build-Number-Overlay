369aad473e39829c4b75c6799ea9c7f13fe910de
Madushan Gamage <madushanbsns@gmail.com>
2015-09-22 10:52:28 +0800

    included screenshot

--- BuildNumberTagger/AppDelegate.swift
+++ BuildNumberTagger/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  BuildNumberTagger
 //
 //  Created by Madushan Gamage on 9/22/15.
-//  Copyright (c) 2015 Hapticus. All rights reserved.
+//  Copyright (c) 2015 Madnik. All rights reserved.
 //
 
 import UIKit
